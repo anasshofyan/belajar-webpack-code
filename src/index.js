d88541c22a31b72fdf953febd0694c3753a7976b
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2023-08-25 13:45:06 +0700

    feat: add loader css, style, babek

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {run} from'./app/app.js';
+require('./style.css');
+import { run } from './app/app.js';
 import {AlertService} from'./app/alert.service.js';
 import {CalculatorService} from'./app/calculator.service.js';
 import {JokesService} from'./app/jokes.service.js';
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,24 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'output'),
         filename: 'bundle.js'
-    }
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:["style-loader", "css-loader"]
+            },
+            {
+                test: /\.m?.js$/, // mencari file yang berakhiran .mjs atau .js
+                exclude: /(node_modules|bower_components)/, // jika ada file yang ada di node_modules atau bower_components, maka file tersebut tidak akan di compile
+                use: {
+                    loader: 'babel-loader', // menggunakan babel-loader
+                    options: {
+                        presets: ['@babel/preset-env'] // menggunakan preset-env
+                    }
+                }
+            }
+        ]
+    },
 };
